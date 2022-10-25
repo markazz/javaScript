5a6da9613de1114fa16fb39a882fd262abf3ff0b
Mark Rasheed <mark.rasheed85@hotmail.com>
2022-10-25 19:37:52 +0100

    Sample Event Delegation

---
+++ eventDelegation.js
@@ -0,0 +1,28 @@
+// TASK 1
+// create function to get the target 
+
+
+
+
+
+
+// TASK 2
+//create function to remove item 
+
+
+
+
+
+// TASK 3
+//  prevent the button to take you else where 
+
+
+
+// TASK 4
+//  add event listener to the buttons
+
+
+
+
+
+
