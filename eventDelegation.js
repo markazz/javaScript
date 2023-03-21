c1fa58739b0929ee293d7ebe3db8b16840a6cb45
Mark Rasheed <mark.rasheed85@hotmail.com>
2023-03-21 19:48:45 +0000

    added functions & event listener

--- eventDelegation.js
+++ eventDelegation.js
@@ -1,26 +1,46 @@
 // TASK 1
 // create function to get the target 
+function getTheTargetElement(e){
 
-
-
-
+    return e.target;
+}
 
 
 // TASK 2
 //create function to remove item 
+function removeItem(e){
 
+// create the variables 
+var target, parent, grandParent;
 
+// get the item Clicked 
+target = getTheTargetElement(e);
 
+// get its list item 
+parent = target.parentNode;
 
+// get its list  
+grandParent = target.parentNode.parentNode;
+
+// remove the element
+grandParent.removeChild(parent);
+
+}
 
 // TASK 3
 //  prevent the button to take you else where 
-
+e.preventDefault();
 
 
 // TASK 4
-//  add event listener to the buttons
-var a= 10;
+//  add event listener to the items 
+
+// get the html element 
+var el = document.getElementById('myUserName');
+
+el.addEventListener('click', function(e){
+    removeItem(e);
+}, false);
 
 
 
