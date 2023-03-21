5ef4c189cf1044afae9bbdff8909279eb39b93d0
Mark Rasheed <mark.rasheed85@hotmail.com>
2023-03-21 20:03:05 +0000

    added focus() method

---
+++ onLoad.js
@@ -0,0 +1,14 @@
+
+function willLoad() {
+    
+    // get the HTML element
+    var el = document.getElementById('getTheUsername');
+
+    // adding focus
+    el.focus();
+}
+
+// add event listener 
+window.addEventListener('load', willLoad , false);
+
+
